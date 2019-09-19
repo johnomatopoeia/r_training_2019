7088cb727a43b950429f834d16f4f2ca17d0fd14
johnomatopoeia <johnjzugelder@gmail.com>
2019-09-20 05:59:43 +1000

    Reordering Content for initial R Scrip

--- 01_RStudio_Orientation.R
+++ 01_RStudio_Orientation.R
@@ -1,14 +1,8 @@
-# Place your cursor on the install packages line and press Ctrl+Enter or Cmd+Enter
+# Place your cursor on the first line and press Ctrl+Enter or Cmd+Enter
 # Notice how the cursor advanced forward one line
+# Also notice the output in the console window
 
-install.packages('tidyverse')
-
-# Pressing Ctrl+Alt+E or Cmd+Option+E runs the rest of of the code
-install.packages('ggthemes')
-install.packages("shiny")
-install.packages("lubridate")
-install.packages("stringr")
-install.packages("snakecase")
+print("hello world!")
 
 # Keyboard shortcuts are the most efficient way to perform common tasks during development, and the RStudio 
 # displays a nice list when you press Alt+Shift+K or Option+Shift+K
@@ -18,5 +12,14 @@ install.packages("snakecase")
 # Adding to the two shortcuts above, you can navigate the panes with Ctrl+1 or Cmd+1 numbers 1 to 0 will activate each of the objects in the panes
 # Ctrl+Shift+1 or Cmd+Shift+1 will activate and zoom to a solo view of the pane objects
 
-# Now that we have the essential packages, let's start with the syntax and operations in the second code file
+# Pressing Ctrl+Alt+E or Cmd+Option+E runs the the remaining script instead of just the current line
+# Try pressing Ctrl+Alt+E or Cmd+Option+E now, and look around RStudio to see what happens
+?print
 
+# Note the comments all printed in the console, and the help pain is now displaying information regarding the print function
+
+# Replace the ___ with your name in the next line
+name <- ___
+
+#
+print(paste0("hello ", name,"! Welcome to R training :)"))
